bc30df5b14423086c28a18af8feb13e3af25e27e
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-07-27 13:56:20 -0400

    Circle generation works and you can get into a message board

--- server/publish/circles/circles_index.js
+++ server/publish/circles/circles_index.js
@@ -3,5 +3,6 @@
 /*****************************************************************************/
 
 Meteor.publish('circles_index', function (id) {
-  return Circles.find({_id: id});
+  // return different circle depending on if id was passed or not
+  return Circles.find( (id) ? {_id: id} : {} );
 });
